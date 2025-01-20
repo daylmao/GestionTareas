c3d40f6074188cb147d103bb1017c43f93523b79
Dayron <dayronbp06@gmail.com>
2025-01-20 18:46:43 -0400

    add domain layer

---
+++ GestionTareas.Core.Domain/Entities/Tarea.cs
@@ -0,0 +1,13 @@
+﻿using GestionTareas.Enum;
+
+namespace GestionTareas.Core.Domain.Entities
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public DateOnly DueDate { get; set; }
+        public Status? Status { get; set; }
+        public int AdditionalData { get; set; }
+    }
+}
