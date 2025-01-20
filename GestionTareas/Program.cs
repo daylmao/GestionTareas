2780a5c0c8706ec0a4717d9eccd8a0daa99190af
Dayron <dayronbp06@gmail.com>
2025-01-20 19:38:26 -0400

    update program

--- GestionTareas/Program.cs
+++ GestionTareas/Program.cs
@@ -1,21 +1,18 @@
-using GestionTareas.Context;
-using GestionTareas.Mapper;
 using GestionTareas.Middleware;
-using GestionTareas.Core.Application.Service;
-using Microsoft.EntityFrameworkCore;
-using GestionTareas.Core.Application.Interfaces.Service;
+using GestionTareas.Core.Application;
+using GestionTareas.Infraestructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var config = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(MapperProfile));
-builder.Services.AddDbContext<GestorTareasContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("GestionTareasConnection")));
-builder.Services.AddScoped<ITareaService, TareaService>();  
+builder.Services.AddPersistenceMethod(config);
+builder.Services.AddApplicationMethod();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
