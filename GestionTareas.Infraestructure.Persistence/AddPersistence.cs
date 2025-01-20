2bd5756a5065a535bcb12ec5db1456ca9d5fa8b3
Dayron <dayronbp06@gmail.com>
2025-01-20 18:47:06 -0400

    add persistence layer

---
+++ GestionTareas.Infraestructure.Persistence/AddPersistence.cs
@@ -0,0 +1,33 @@
+﻿using GestionTareas.Context;
+using GestionTareas.Core.Application.Interfaces.Repository;
+using GestionTareas.Infraestructure.Persistence.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionTareas.Infraestructure.Persistence
+{
+    public static class AddPersistence
+    {
+        public static void AddPersistenceMethod(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region connection
+            services.AddDbContext<GestorTareasContext>(b =>
+            {
+                b.UseSqlServer(configuration.GetConnectionString("GestorTareasConnection"),
+                c => c.MigrationsAssembly(typeof(GestorTareasContext).Assembly.FullName));
+            });
+            #endregion
+
+            #region repository
+            services.AddTransient<ITareaRepository, TareaRepository>();
+
+            #endregion
+        }
+    }
+}
