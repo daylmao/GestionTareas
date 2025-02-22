45978c20c0c51d6a55ee253b45770cfffcb16c3d
Dayron <dayronbp06@gmail.com>
2025-02-22 19:37:09 -0400

    add optimizacion

--- GestionTareas.Core.Application/Interfaces/Repository/ITareaRepository.cs
+++ GestionTareas.Core.Application/Interfaces/Repository/ITareaRepository.cs
@@ -5,6 +5,9 @@ namespace GestionTareas.Core.Application.Interfaces.Repository
 {
     public interface ITareaRepository : IGenericRepository<Tarea>
     {
+<<<<<<< HEAD
         bool Validate(Func<Tarea, bool> validate);
+=======
+
     }
 }
